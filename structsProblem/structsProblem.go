fd8728fc7e76ca5a5ea7bb669d41bc4acecd6a6b
Rohit Awasthi <rohit00082002@gmail.com>
2014-04-21 17:50:50 +0530

    More on Struct

--- structsProblem/structsProblem.go
+++ structsProblem/structsProblem.go
@@ -1,11 +1,43 @@
 package main
-import "fmt"
-
-type Circle struct{
- r,x,y float32
+import (
+	"fmt"; "math"
+)
+/*
+type Shape interface {
+ area() float64
 }
+*/
+
+// Circle Struct has scope outside of package, because it starts with Capital Letter.
+type Circle struct{
+ r,x,y float64
+}
+
+type Rectangle struct {
+ w,h float64
+}
+
+func ( rectangle *Rectangle) area() float64 {
+ return rectangle.w * rectangle.h
+}
+
+func ( circle *Circle) area() float64 {
+ return math.Pi * circle.r * circle.r
+}
+
+
+
 func main() {
- var circle Circle;
- circle = Circle{r:10,x:12,y:35}
- fmt.Printf("Circle's Radius and Center(x,y) are::%f",circle.x)
+ var circle Circle
+ var rectangle Rectangle
+
+ circle = Circle{r:7,x:12,y:35}
+ rectangle = Rectangle{w:10, h:25}
+
+ circleArea := circle.area()
+ fmt.Println("AREA   ", circleArea)
+
+ rectangleArea := rectangle.area()
+ fmt.Println("Rectangle area   ", rectangleArea)
+
 }
